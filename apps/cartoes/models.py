476f07a3406b80df412d489e23c4834816cf0189
Guilherme <guifigueiredo.gfd@gmail.com>
2025-05-04 12:16:31 -0300

    adicionando campo banco no model para o app de bancos

--- apps/cartoes/models.py
+++ apps/cartoes/models.py
@@ -7,6 +7,7 @@ class Cartao(models.Model):
     nome = models.CharField(max_length=100)
     bandeira = models.CharField(max_length=100)
     limite = models.FloatField()
+    banco = models.CharField(max_length=100, default='')
     dia_pagamento = models.IntegerField()
     dia_fechamento = models.IntegerField()
     usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='cartoes')
--- apps/cartoes/views.py
+++ apps/cartoes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def cartoes(request):
+    return render(request, 'cartoes.html')
